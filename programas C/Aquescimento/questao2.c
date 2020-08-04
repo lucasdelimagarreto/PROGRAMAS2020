e5c6a0bc87e7e1ca4096053ab2c3e002b4a4f7c6
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2020-08-04 14:13:27 -0300

    sad

--- programas C/Aquescimento/questao2.c
+++ programas C/Aquescimento/questao2.c
@@ -3,5 +3,21 @@
 
 int main (){
 
-    
+    int quantidade, prox;
+    int anter = 1;
+    int atual = 1;
+    int cont = 2;
+
+    printf("Quantos numeros da sequencia deseja?");
+    scanf("%d", &quantidade);
+
+    printf("1 1");
+    while (cont < quantidade){
+        prox = anter + atual;
+        anter = atual;
+        atual = prox;
+        cont = cont + 1;
+        printf(" %d",prox);
+    }
+    return 0;
 }
