f55b5059b510997224878dfa18037557f2095ffb
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2020-08-12 22:30:49 -0300

    sad

--- programas C/Aquescimento/questao3_vetores_matrizes.c
+++ programas C/Aquescimento/questao3_vetores_matrizes.c
@@ -3,30 +3,38 @@
 
 int main (){
 
-    int matriz[10][3], aux = 0, aux2 = 0, linha1 = 0, linha2 = 0;
+    int matriz[10][3], aux = 0, aux2 = 0, linha1 = 0, linha2 = 0, linha3 = 0, contE = 0, contD = 0;
 
     while (aux != -1){
 
         printf("Digite os valores que serão adicionados na matriz: ");
         scanf("%d", &aux);
 
-        if (aux == -1){
-
-            aux = 0;
-        }
-
-        if (aux % 2 == 1){
-
-            matriz[linha1][0] = aux;
-            linha1++;
+        if (contE == 9 || contD ==9){
             
-        }
-
-        else{
-            
-            matriz[linha2][2] = aux;
+            matriz[linha2][1] = aux;
             linha2++;
+        
+        }
+        
+        else{
 
+            if (aux % 2 == 1){
+
+                matriz[linha1][0] = aux;
+                linha1++;
+                contE++;
+            
+            }
+
+            else{
+            
+                matriz[linha3][2] = aux;
+                linha3++;
+                contD++;
+
+            }
+        
         }
         
     } 
