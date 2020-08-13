61f3702c6ad817f66415d67d1caf48a0fefa2214
lucasdelimagarreto <54108848+lucasdelimagarreto@users.noreply.github.com>
2020-08-12 21:56:48 -0300

    sad

--- programas C/Aquescimento/questao2_vetores_matrizes.c
+++ programas C/Aquescimento/questao2_vetores_matrizes.c
@@ -3,32 +3,56 @@
 
 int main (){
 
-    //Construa um programa que receberá valores inteiros em quantidade suficiente para montar uma matriz 5x5.
-    // Em seguida, o algoritmo deverá escrever a matriz na ordem em que foi montada 
-    //(da esquerda para direita, de cima para baixo).
-    // Ao final, acrescente uma linha em branco e, então, faça a impressão da matriz transposta correspondente.
+    int matriz[3][3], i, j, aux;
 
-    int matriz[3][3];
-
-    for (int i = 0; i < 3; i++){
+    for ( i = 0; i < 3; i++){
         
-        for (int j = 0; j < 3; j++){
+        for ( j = 0; j < 3; j++){
 
-            printf("Digite o numero da linha %d e coluna %d: ", i + 1, j + 1);
-            scanf("%d", &matriz[i][j]);
-
+            printf("Digite o numero da linha %d e coluna %d: ", i, j);
+            scanf("%d", &aux);
+            matriz[i][j] = aux;
+            
         }
     }
-    
-    for (int i = 0; i < 3; i++){
 
-        for (int j = 0; j < 3; j++){
+    for ( i = 0; i < 3; i++){
 
-            printf("%d ", &matriz[i][j]);
+        for ( j = 0; j < 3; j++){
+
+            printf("%d ", matriz[i][j]);
         }
+        
+        printf("\n");
+    }
+
+    printf("\n*********************************\n\n");
+
+    for (i = 0; i < 3; i++){
+
+        for (j = 0; j < 3; j++){
+
+            if (i > j){
+
+                aux = matriz[i][j];
+                matriz[i][j] = matriz[j][i];
+                matriz[j][i] = aux;
+
+            }
+            
+        }
+        
+    }
+
+    for ( i = 0; i < 3; i++){
+
+        for ( j = 0; j < 3; j++){
+
+            printf("%d ", matriz[i][j]);
+        }
+        
         printf("\n");
     }
     
-
     return 0;
 }
---
+++ programas C/Aquescimento/questao3_vetores_matrizes.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    int matriz[10][3], aux = 0, aux2 = 0, linha1 = 0, linha2 = 0;
+
+    while (aux != -1){
+
+        printf("Digite os valores que serão adicionados na matriz: ");
+        scanf("%d", &aux);
+
+        if (aux == -1){
+
+            aux = 0;
+        }
+
+        if (aux % 2 == 1){
+
+            matriz[linha1][0] = aux;
+            linha1++;
+            
+        }
+
+        else{
+            
+            matriz[linha2][2] = aux;
+            linha2++;
+
+        }
+        
+        
+    }
+    
+
+    return 0;
+}
